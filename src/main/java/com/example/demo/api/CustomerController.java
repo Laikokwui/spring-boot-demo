42f344e706aff425c1cbff9d28535d81cfa84c33
Laikokwei <kokwei325@gmail.com>
2022-09-19 11:25:42 +0800

    add mapping for customer CRUD

--- src/main/java/com/example/demo/api/CustomerController.java
+++ src/main/java/com/example/demo/api/CustomerController.java
@@ -1,23 +1,31 @@
 package com.example.demo.api;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.repository.CustomerRepository;
-
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/customer")
 public class CustomerController {
-    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-    private final CustomerRepository customerRepository;
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String getCustomers() {
+    return "view list of Customer";
+  }
 
-    public CustomerController(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
+  @RequestMapping(value = "/create", method = RequestMethod.POST)
+  public String createCustomer() {
+    return "create Customer";
+  }
+
+  @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+  public String updateCustomer() {
+    return "update Customer";
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public String getCustomer() {
+    return "view Customer";
+  }
 }
+
