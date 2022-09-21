2ad4b2be8040ef5958b55b8024a185a2093798de
Laikokwei <kokwei325@gmail.com>
2022-09-21 17:21:53 +0800

    modify customer controller

--- src/main/java/com/example/demo/api/CustomerController.java
+++ src/main/java/com/example/demo/api/CustomerController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
@@ -45,9 +43,9 @@ public class CustomerController {
     }
   }
   
-  @GetMapping("/customer/{id}")
+  @GetMapping("/customer/hello")
   public String getCustomerById(@PathVariable("id") String id) {
-    return "view Customer";
+    return "customer/index.html";
   }
 
   @PostMapping("/customer/create")
